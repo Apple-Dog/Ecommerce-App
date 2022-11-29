a8ff2303a65f109174eafab8e47f6b76b7d2433d
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-11-29 11:26:43 +0530

    Added Email Field in User Schema

--- models/user.Schema.js
+++ models/user.Schema.js
@@ -2,10 +2,16 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
     {
-        name:{
+        name : {
             type : String,
             required : [true,"Name is Required."],
             maxLength : [50,"Name must be less than 50."], 
+        },
+        email : {
+            type : String,
+            required : [true,"Name is Required."],
+            unique: true,
         }
+
     }
 )
