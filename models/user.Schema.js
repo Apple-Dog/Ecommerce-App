61ce1d439724e85caff8d7b6ec3c78f5ad400cd0
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-11-29 20:25:48 +0530

    Added PWD Encryption in  Schema, Schema Methods,Set up .env

--- models/user.Schema.js
+++ models/user.Schema.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
+import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
+import config from "../config/index"
 
+// User Schema
 const userSchema = mongoose.Schema(
     {
         name : {
@@ -7,6 +13,7 @@ const userSchema = mongoose.Schema(
             required : [true,"Name is Required."],
             maxLength : [50,"Name must be less than 50."], 
         },
+
         email : {
             type : String,
             required : [true,"Email is Required."],
@@ -20,5 +27,52 @@ const userSchema = mongoose.Schema(
             select : false, 
         },
 
-    }
-)
+        role : {
+            type : String,
+            enum : Object.values(AuthRoles),
+            default : AuthRoles.USER,
+        },
+
+        forgotPasswordToken : String,
+        forgotPasswordExpiry : Date,
+        
+
+    },
+    {
+        timestamps : true,
+    },
+);
+
+export default mongoose.model("User",userSchema);
+
+//Password Encryption in Schema
+
+userSchema.pre("save", async function(next){
+    if(!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+});
+
+// Add More Features Directly into Schema
+
+userSchema.methods = {
+    // Compare Password
+    comparePassword :  async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password);
+    },
+
+    // Generate JWT TOKEN
+    getJwtToken : function(){
+        return JWT.sign(
+            {
+                _id : this._id,
+                role : this.role,
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn : config.JWT_EXPIRY,
+            }
+        );
+    },
+};
+
