32360daf40de6a73ab4110bf35c41c0618ac1260
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-05 13:25:21 +0530

    changes made in UserSchema  - Added More Fields

--- models/product.Schema.js
+++ models/product.Schema.js
@@ -4,10 +4,54 @@ const productSchema = new mongoose.Schema(
     {
         name : {
             type : String,
+            required : [true,"Please provide a product name."],
+            trim : true,
+            maxLength : [120,"Product name cannot exceed 120 characters."], 
         },
+
+        price : {
+            type : Number,
+            required : [true,"Please provide a product price."],
+            maxLength : [6,"Product price cannot exceed 6 figure."], 
+        },
+
+        description : {
+            type : String,
+            default:null,
+            trim : true,
+            minLength : [,"Product description cannot be less than 3 characters."], 
+            //Used For Editor in Future - Changes can be made.
+        },
+        
+        photos : [
+            {
+                secure_url : {
+                    type : String,
+                    required : true,
+                }
+            }
+        ],
+
+        stock : {
+            type : Number,
+            default : 0,
+        },
+
+        sold : {
+            type : Number,
+            default : 0,
+        },
+        
+        collectionId : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Collection",
+        },
+        
     },
 
     {
         timestamps : true,
-    }
-)
+    },
+);
+
+export default mongoose.model("Product",productSchema);
