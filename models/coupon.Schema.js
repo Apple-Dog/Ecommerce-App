7b46072f59ceff02f3abf35f7f2a2b0fd03fc5e8
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-05 12:47:04 +0530

    Added Coupon Schema in Models

---
+++ models/coupon.Schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+const couponSchema = new mongoose.Schema(
+    {
+        code : {
+            type : String,
+            required : [true,"Please provide coupon name."],
+        },
+
+        discount : {
+            type : Number,
+            default : 0,
+        },
+
+        active : {
+            type : Boolean,
+            default : true
+        },
+
+    },
+    
+    {
+        timestamps : true,
+    }
+)
