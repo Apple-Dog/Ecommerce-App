12810fb5b5ab4b9219daec4578524c2d7e06e20a
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-06 14:41:29 +0530

    Added Order Status and Payment Mode Enums in utils

---
+++ utils/paymentModes.js
@@ -0,0 +1,10 @@
+const PaymentModes = Object.freeze({
+    COD : "COD",
+    DEBITCARD : "DEBITCARD",
+    CREDITCARD : "CREDITCARD",
+    NETBANKING : "NETBANKING",
+    UPI : "UPI",
+    WALLET : "WALLET",
+});
+
+export default PaymentModes;
