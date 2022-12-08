d11a1e6f46c96f38e90030b5c1e01e3162e47a4a
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-08 14:15:24 +0530

    Some changes in Comments in app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import morgan from "morgan";
 const app = express();
 
 
-//Middleware's
+//Mounting Middleware's
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors());
