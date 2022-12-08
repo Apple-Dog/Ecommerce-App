f52cf01e2b58800e532de6f1354bc404ef298213
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-08 11:42:27 +0530

    configured Middleware in app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
+//Initializing Express
 const app = express();
+
+
+//Middleware's
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cors());
+app.use(cookieParser());
+
+//Morgan Logger
+app.use(morgan("tiny"));
+
 
 export default app;
