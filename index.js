a52b57b1300f21552570d4bad8cf65e3eb7358b7
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-08 11:16:53 +0530

    Added Index.js and configure DB connection

---
+++ index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import app from "./app";
+import  config  from "./config/index";
+
+// Self Invoking Function For Database Connection
+(async () => {
+    // Runs As soon as the app starts
+    try {
+
+       await mongoose.connect(config.MONGODB_URL);
+        console.log("Connected tro DB Successfully");
+
+        app.on("error", (error)=>{
+            console.log(error);
+            throw error;
+        });
+
+        const onListening = () => {
+            console.log(`Listening on PORT : $(config.PORT)`);
+        };
+
+        app.listen(config.PORT, onListening);
+        
+    } catch (error) {
+        console.log ("ERROR", error);
+        throw error;
+    }
+})();
