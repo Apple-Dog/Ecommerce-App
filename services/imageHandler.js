534c631ad07456a4d32174c3a7bb4446a1c0ad4e
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-01-09 05:07:18 +0530

    Code Refactor Function Name Changed

--- services/imageHandler.js
+++ services/imageHandler.js
@@ -1,6 +1,6 @@
 import s3 from "../config/S3.config";
 
-export const s3FileUpload = async ({bucketName, key, body, contentType}) => {
+export const uploadFile = async ({bucketName, key, body, contentType}) => {
     return await s3.upload({
         Bucket : bucketName,
         Key : key,
