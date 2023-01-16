3438f32c956c3d30365e37dcb54b6512d1e85d99
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-01-17 02:47:37 +0530

    Added Payment Status in utils

---
+++ utils/paymentStatus.js
@@ -0,0 +1,11 @@
+const PaymentStatus = Object.freeze({
+    CREATED : "created",
+    AUTHORIZED : "authorized",
+    CAPTURED : "captured",
+    REFUNDED : "refunded",
+    FAILED : "failed",
+});
+
+export default PaymentStatus;
+
+// Enum Created For Payment Status
