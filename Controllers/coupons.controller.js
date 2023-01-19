e64d16227827f0b3c62a37157f363500f5cb888d
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-01-19 11:20:44 +0530

    Added Code for Create Coupon, Deactivate Coupon, Delete Coupon, Get All Coupons in coupons.controller.js

--- Controllers/coupons.controller.js
+++ Controllers/coupons.controller.js
@@ -1,3 +1,9 @@
+import Coupon from "../models/coupon.Schema";
+import asyncHandler from "../services/asyncHandler";
+import CustomError from "../utils/customError";
+
+
+
 /**********************************************************
  * @CREATE_COUPON
  * @REQUEST_TYPE POST
@@ -7,6 +13,47 @@
  * @Parameters code, discount
  * @Returns Coupon Object with success message "Coupon Created SuccessFully"
  *********************************************************/
+
+export const createCoupon = asyncHandler (async (req, res) => {
+
+    // Grab Code and Discount from the Frontend
+    const { code, discount } = req.body;
+
+    // Validate if code and discount exits
+    if(!code || !discount){
+        throw new CustomError("Please Fill All Fields",400);
+    };
+
+    // Check If Coupon Code already Exists
+    const existingCoupon = await Coupon.findOne({code});
+
+    if(existingCoupon){
+        throw new CustomError("Coupon Already Exists.",400);
+    };
+
+    // Creating New Coupon Entry in the Database
+    const coupon = await Coupon.create({
+        code,
+        discount,
+        active,
+    });
+
+    // Sending Response if Coupon Entry gets Sucessfully Created in the Database
+    res.status(200).json({
+        success : true,
+        message : "Coupon Created Successfully",
+        coupon,
+    });
+
+    // Unsetting coupon, existingCoupon to Free Up Space from the Memory
+    existingCoupon.remove();
+    coupon.remove();
+
+});
+
+
+
+
 
 
 /**********************************************************
@@ -19,16 +66,93 @@
  * @Returns Coupon Object with success message "Coupon Deactivated SuccessFully"
  *********************************************************/
 
+export const deactivateCoupon = asyncHandler (async (req, res) => {
+
+    // Grab Coupon ID from URL
+    const { couponId } = req.params;
+
+    // Check whether Coupon Exist in the Database or Not (Check For Valid Coupon)
+    const coupon = await Coupon.findOne({_id : couponId});
+
+    if(!coupon){
+        throw new CustomError("Coupon does not Exists",404);
+    };
+
+    // Check whether Coupon already Deactivated or Not (Check For Valid Coupon)
+    if (coupon.active === false){
+        throw new CustomError("Coupon already Deactivated",404);
+    };
+
+    // Setting Coupon to Deactive State & Save it in the Database
+    const active = false;
+
+    const deactivatedCoupon = await Collection.findByIdAndUpdate(
+        couponId,
+        {
+            active,
+        },
+
+        {
+            new : true,
+            runValidators : true,
+        }
+    );
+
+    // Sending Response if Coupon Deactivated Sucessfully Created in the Database
+    res.status(200).json({
+        success : true,
+        message : "Coupon Deactivated Successfully",
+        deactivatedCoupon,
+    });
+
+    // Unsetting coupon, deactivatedCoupon to Free Up Space from the Memory
+    coupon.remove();
+    deactivatedCoupon.remove();
+
+}); 
+
+
+
+
+
 
 /**********************************************************
  * @DELETE_COUPON
- * @REQUEST_TYPE GET
+ * @REQUEST_TYPE DELETE
  * @Route https://localhost:5000/api/coupon/:couponId
  * @Description 1. Controller used for deleting the coupon
  *              2. Only admin and Moderator can delete the coupon
  * @Parameters ID
  * @Returns Success Message "Coupon Deleted SuccessFully"
  *********************************************************/
+
+export const deleteCoupon = asyncHandler (async (req, res) => {
+
+    // Grab Coupon ID from URL
+    const { couponId } = req.params;
+
+    // Delete an Existing Collection in the Database.
+    const deletedCoupon = await Coupon.findByIdAndDelete({_id : couponId});
+
+    if(!coupon){
+        throw new CustomError("Coupon does not Exists",404);
+    };
+
+    // Sending Response if Coupon Deactivated Sucessfully Created in the Database
+    res.status(200).json({
+        success : true,
+        message : "Coupon Deleted Successfully",
+        deletedCoupon,
+    });
+
+    // Unsetting deletedCoupon to Free Up Space from the Memory
+    deletedCoupon.remove();
+
+}); 
+
+
+
+
 
 
 
@@ -41,3 +165,26 @@
  * @Parameters ID
  * @Returns allCoupons Object
  *********************************************************/
+
+
+export const getAllCoupons = asyncHandler (async (req, res) => {
+    
+    // Gets A List Of Coupons and Store in "allCoupons".
+    const allCoupons = await Collection.find();
+
+    // Validate Whether "allCoupons" is Empty or Not.
+    if(!allCoupons){
+        throw new CustomError("No Coupons Found",404);
+    };
+
+    // Sending Response if Coupon List gets Sucessfully Fetched from the Database
+    res.status.json({
+        success : true,
+        message : "Fetched All Coupons Successfully",
+        allCoupons, 
+    });
+
+    // Unsetting allCoupons to Free Up Space from the Memory
+    allCoupons.remove();
+});
+
